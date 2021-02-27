c3a23bd257bb6fb46c58f3b2c727b933b8edce3d
phokionas <79746893+phokionas@users.noreply.github.com>
2021-02-27 13:20:47 +0000

    Deploying to gh-pages from master @ 20a69d92092a68afc46a4632e2b83de1906d5b0d 🚀

---
+++ Bmrfloor014.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="Bmrfloor014" tilewidth="32" tileheight="32" tilecount="16" columns="16">
+ <image source="Bmrfloor014.png" width="520" height="60"/>
+</tileset>
---
+++ Vikings_tilemaps_026.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="2021.02.15" name="Vikings_tilemaps_026" tilewidth="32" tileheight="32" tilecount="60" columns="10">
+ <image source="Vikings_tilemaps_026.bmp" width="320" height="200"/>
+</tileset>
